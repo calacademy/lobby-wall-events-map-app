e80946b002eeca1370e2787741108e5adc25c230
Josh Ause <joshua.ause@gmail.com>
2017-08-22 10:40:55 -0700

    remove unnecessary div wrapper from event list body

--- src/js/components/EventList.jsx
+++ src/js/components/EventList.jsx
@@ -47,9 +47,7 @@ var Event = React.createClass({
             <span className="meridiem">{this._getMeridiem(this.props.time)}</span></h2>
             <h1>{this.props.dataset.title}</h1>
             <span className="location">{this.props.dataset.location}</span>
-            <div>
-              <p dangerouslySetInnerHTML={markup} />
-            </div>
+            <p dangerouslySetInnerHTML={markup} />
           </article>
         )
 
